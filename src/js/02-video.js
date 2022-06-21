a034eb6c0e04519a7574f9b9cdca7252452aeeaa
Aleksandr Raskulov <alexandr1991r@gmail.com>
2022-06-21 23:33:33 +0300

    upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+console.log(throttle);
+console.log(Player);
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const lastPosition = localStorage.getItem('videoplayer-current-time');
+if (lastPosition) {
+  player.setCurrentTime(lastPosition);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', onSubmit);
+form.addEventListener('input', throttle(onInput, 500));
+
+const dataResult = {};
+initForm();
+
+//
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const { email, message } = event.target.elements;
+
+  if (!email.value || !message.value) {
+    return alert('Будь ласка заповніть всі поля!');
+  }
+
+  form.reset();
+  localStorage.clear();
+  console.log(dataResult);
+}
+
+//
+
+function onInput(event) {
+  dataResult[event.target.name] = event.target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(dataResult));
+}
+
+//
+
+function initForm() {
+  let initValues = localStorage.getItem('feedback-form-state');
+
+  if (initValues) {
+    initValues = JSON.parse(initValues);
+
+    Object.entries(initValues).forEach(([name, value]) => {
+      dataResult[name] = value;
+      form.elements[name].value = value;
+    });
+  }
+}
